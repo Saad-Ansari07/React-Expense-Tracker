57a63f9e4dfc34e66bd160b1c78af9c2cd5aa588
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-06-22 13:22:05 +0500

    Update AppReducer.js

--- src/Context/AppReducer.js
+++ src/Context/AppReducer.js
@@ -1,18 +1,16 @@
 export default (state, action) => {
-switch(action.type) {
-    case 'DELETE-TRANSACTION':
-        return {
-            ...state,
-            transactions: state.transactions.filter(transaction => transaction.id 
-                !== action.payload)
-        }
+    switch(action.type) {
+        case 'DELETE-TRANSACTION':
+            return {
+                ...state, 
+                transactions: state.transactions.filter(transactions => transactions.id !== action.payload)
+            }
         case 'ADD-TRANSACTION':
-        return {
-            ...state,
-            transactions: [action.payload, ...state.transaction]
-               
-        }
-    default: 
-    return state
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+        default:
+            return state;
+    }
 }
-}; 
