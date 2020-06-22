7e70ec699439cdafd807ff9d8147e9622aac3327
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-06-22 12:17:53 +0500

    Update balance.js

--- src/components/balance.js
+++ src/components/balance.js
@@ -9,14 +9,14 @@ export const Balance = () => {
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2); 
   
     return (
-        <div>
+        <>
             <h4>
               Your Balance  
             </h4>
             <h1>
                ${total}
             </h1>
-        </div>
+        </>
     )
 };
 
