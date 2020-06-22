c03ab7b4a81f6b250407a93d5a5331fc7e5d8e3d
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-06-22 12:16:55 +0500

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -9,11 +9,11 @@ import { GlobalContext } from '../Context/GlobalState';
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
     return (
-        <div>
+        <>
       <h3>History</h3>
       <ul className="list">
        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))};
       </ul>
-      </div>
+      </>
     )
 }
